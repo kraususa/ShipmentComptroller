258f69d06ded3c7e38e0d396820d2276c5abadbb
Avinash <apatil@kraususa.com>
2013-08-06 10:26:22 +0530

    New report pages added

---
+++ ShippingController(V1.0)/ShippingController(V1.0)/Forms/Web Forms/AutoCompleteService.aspx.cs
@@ -0,0 +1,39 @@
+﻿using PackingClassLibrary.CustomEntity;
+using ShippingController_V1._0_.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShippingController_V1._0_.Forms.Web_Forms
+{
+    public partial class AutoCompleteService : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        [System.Web.Services.WebMethod]
+        [System.Web.Script.Services.ScriptMethod()]
+        public static List<string> SearchpackingID(string prefixText, int count)
+        {
+            List<string> lsreturn = new List<string>();
+            if (prefixText == "")
+            {
+                prefixText = "SH";
+            }
+            List<cstPackingTbl> lspcking = cGlobal.call.GetPackingTbl();
+            foreach (var packing in lspcking)
+            {
+
+                if (packing.ShippingNum.Contains(prefixText))
+                {
+                    lsreturn.Add(packing.ShippingNum.ToString().ToUpper());
+                }
+            }
+            return lsreturn;
+        }
+    }
+}
---
+++ ShippingController(V1.0)/ShippingController(V1.0)/Forms/Web Forms/rptPacking.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Forms/Master Forms/Admin.Master" AutoEventWireup="true" CodeBehind="rptPacking.aspx.cs" Inherits="ShippingController_V1._0_.Forms.Web_Forms.Packing" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
