d3bdf26c22bd92e672db10d1044fc63f4a878079
Avinash <apatil@kraususa.com>
2014-02-04 22:28:34 +0530

    Config file added

--- ShippingController(V1.0)/ShippingController(V1.0)/Forms/Web Forms/frmRMAConfig.aspx.cs
+++ ShippingController(V1.0)/ShippingController(V1.0)/Forms/Web Forms/frmRMAConfig.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace ShippingController_V1._0_.Forms.Web_Forms
 {
-    public partial class frmRGAReasons : System.Web.UI.Page
+    public partial class frmRMAConfig : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
